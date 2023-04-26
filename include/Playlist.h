ff4e47c85c6cfc244a2544c0bc2afba2be513af2
glooooh <glorinhamsouza@gmail.com>
2023-04-26 11:36:51 -0300

    Classes da lista ligada completas

--- include/Playlist.h
+++ include/Playlist.h
@@ -1,23 +1,37 @@
 #include <string>
 using namespace std;
 
-#include "listaMusica.h"
+#include "ListaMusica.h"
 
 class Playlist
 {
 private:
     string nome;
-    listaMusica* cabeca;
-    listaMusica* cauda;
+    ListaMusica *lista;
 
 public:
-// Construtores e desconstrutores
+    // Construtores e desconstrutores
     Playlist(string);
     ~Playlist();
 
-// CRUDS
-    void inserir(Musica);
-    void remover(string, string);
-    
+    // CRUD
+    bool adicionarMusica(Musica);
+    bool removerMusica(int); //por índice ou música?
+
+    // Metodos da funcionalidade "tocando"
+    Musica proximaMusica();
+
+    // Impressão com recursão
     void imprimir();
 };
+
+// a) Crie os construtores e destrutores correspondentes à classe
+// b) Crie métodos de adição e remoção de músicas à uma playlist (músicas devem ser
+// armazenadas em uma lista ligada)
+// c) Crie métodos de retornar a próxima música a ser tocada
+//    i) Para cada chamada deste método a música retornada deve ser a próxima da lista de
+//    músicas na playlist (começando pela primeira, depois a segunda e assim por
+//    diante). Caso não haja mais músicas para ser tocadas, o método deve retornar
+//    NULL.
+// d) Crie um método, usando recursão, para imprimir todas as músicas que formam a
+// playlist.
